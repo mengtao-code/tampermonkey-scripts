0e1e61b83493553d36112dd96db6051eebdd5918
noah <noah@onerzone.com>
2023-04-07 16:49:26 +0800

    1

--- inoreader/fix-image-error.js
+++ inoreader/fix-image-error.js
@@ -2,9 +2,11 @@
 // @name             fix-image-error
 // @name:en          fix-image-error
 // @version          0.0.1
-// @namespace        https://github.com/noah-onerzone/tampermonkey-scripts
+// @namespace        https://onerzone.com
 // @description      Fix image load error by requesting to origin download link
-// @description:en   Display anti-hotlinking images, for forced not send a referrer.
+// @author           Noah Xin
+// @license          MIT
+// @supportURL       https://github.com/noah-onerzone/tampermonkey-scripts/blob/master/inoreader/fix-image-error.js
 // @include          http*://*.inoreader.com/*
 // @icon             http://www.inoreader.com/favicon.ico
 // @grant GM_xmlhttpRequest

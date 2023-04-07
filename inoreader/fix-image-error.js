18de14a4efa7330395d9faae46f1d1c099b346fb
noah <xinmengtao@outlook.com>
2023-04-07 16:38:15 +0800

    1

--- inoreader/fix-image-error.js
+++ inoreader/fix-image-error.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name             Shit
-// @name:en          Shit
+// @name             fix-image-error
+// @name:en          fix-image-error
 // @version          0.0.1
-// @namespace        https://github.com/maboloshi/UserScripts/
-// @description      Shit
+// @namespace        https://github.com/noah-onerzone/tampermonkey-scripts
+// @description      Fix image load error by requesting to origin download link
 // @description:en   Display anti-hotlinking images, for forced not send a referrer.
 // @include          http*://*.inoreader.com/*
 // @icon             http://www.inoreader.com/favicon.ico
@@ -11,13 +11,18 @@
 // @connect *
 // ==/UserScript==
 const config = {
-    name: "image-loader",
-    urlPrefix: [
+    name: "fix-image-error",
+    urlPrefix: [ // weibo images prefix
         'https://tvax4.sinaimg.cn'
     ]
 }
 
-
+/**
+ * 
+ * @link https://stackoverflow.com/questions/8778863/downloading-an-image-using-xmlhttprequest-in-a-userscript
+ * @param {*} inputStr 
+ * @returns 
+ */
 function customBase64Encode(inputStr) {
     var
         bbLen = 3,
@@ -35,31 +40,15 @@ function customBase64Encode(inputStr) {
 
     while (inx < inpLen) {
         for (jnx = 0; jnx < bbLen; ++jnx) {
-            /*--- Throw away high-order byte, as documented at:
-              https://developer.mozilla.org/En/Using_XMLHttpRequest#Handling_binary_data
-            */
             if (inx < inpLen)
                 bytebuffer[jnx] = inputStr.charCodeAt(inx++) & 0xff;
             else
                 bytebuffer[jnx] = 0;
         }
-
-        /*--- Get each encoded character, 6 bits at a time.
-            index 0: first  6 bits
-            index 1: second 6 bits
-                        (2 least significant bits from inputStr byte 1
-                         + 4 most significant bits from byte 2)
-            index 2: third  6 bits
-                        (4 least significant bits from inputStr byte 2
-                         + 2 most significant bits from byte 3)
-            index 3: forth  6 bits (6 least significant bits from inputStr byte 3)
-        */
         encodedCharIndexes[0] = bytebuffer[0] >> 2;
         encodedCharIndexes[1] = ((bytebuffer[0] & 0x3) << 4) | (bytebuffer[1] >> 4);
         encodedCharIndexes[2] = ((bytebuffer[1] & 0x0f) << 2) | (bytebuffer[2] >> 6);
         encodedCharIndexes[3] = bytebuffer[2] & 0x3f;
-
-        //--- Determine whether padding happened, and adjust accordingly.
         paddingBytes = inx - (inpLen - 1);
         switch (paddingBytes) {
             case 1:
@@ -74,10 +63,6 @@ function customBase64Encode(inputStr) {
             default:
                 break; // No padding - proceed
         }
-
-        /*--- Now grab each appropriate character out of our keystring,
-            based on our index array and append it to the output string.
-        */
         for (jnx = 0; jnx < enCharLen; ++jnx)
             output += keyStr.charAt(encodedCharIndexes[jnx]);
     }

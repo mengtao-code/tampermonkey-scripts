f36fad83e6c4437653b23e621dda8db70fcb5d6a
mengtao <mengtaoxin@outlook.com>
2023-12-07 15:01:59 +0800

    fix image load

--- inoreader/fix-image-error.js
+++ inoreader/fix-image-error.js
@@ -133,17 +133,17 @@ const processListImage = (dom, mockHeader) => {
     if (dom.getAttribute('process-tag') !== 'doing' && dom.getAttribute('process-tag') !== 'done') {
         dom.setAttribute("process-tag", "doing")
         const style = dom.getAttribute("style")
-        const originUrl = style.substring(style.indexOf("https") + 5).replace("')", "")
+        const originUrl = style.substring(style.indexOf("https")).replace("')", "")
+        console.log(`originUrl:${originUrl}`)
         httpGetRequest(originUrl, mockHeader)
             .then(responseData => {
                 const goodUrl = getImageUrl(responseData);
                 dom.setAttribute('style', `background-image:url('${goodUrl}')`)
+
             })
             .catch(e => console.error(`${config.name} load image failed! ${e}`))
             .finally(() => dom.setAttribute('process-tag', 'done'))
     }
-
-
 }
 
 /**
@@ -159,7 +159,7 @@ const main = () => {
         // 2. list page
         Array.from(
             document.querySelectorAll(`.article_magazine_picture`))
-            .filter(dom=>{dom.getAttribute("style").contains(imageServer)})
+            .filter(dom=>dom.getAttribute("style").includes(imageServer))
             .forEach(image => processListImage(image, mockHeader));
     })
 }
